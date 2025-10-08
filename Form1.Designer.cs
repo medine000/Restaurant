2a8469287b8f65520f19a08541d06694a7969e73
Adın Soyadın <mmusayeva@std.beu.edu.az>
2025-10-08 14:41:52 +0400

    Bütün layihə faylları əlavə edildi

---
+++ Form1.Designer.cs
@@ -0,0 +1,388 @@
+﻿namespace Restaurant
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            pictureBox1 = new PictureBox();
+            pictureBox2 = new PictureBox();
+            pictureBox3 = new PictureBox();
+            pictureBox4 = new PictureBox();
+            pictureBox5 = new PictureBox();
+            pictureBox6 = new PictureBox();
+            pictureBox7 = new PictureBox();
+            pictureBox8 = new PictureBox();
+            pictureBox9 = new PictureBox();
+            pictureBox10 = new PictureBox();
+            pictureBox11 = new PictureBox();
+            pictureBox12 = new PictureBox();
+            pictureBox13 = new PictureBox();
+            listBox1 = new ListBox();
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            button5 = new Button();
+            button6 = new Button();
+            label1 = new Label();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            label2 = new Label();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox4).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox5).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox6).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox7).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox8).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox9).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox10).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox11).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox12).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox13).BeginInit();
+            SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Location = new Point(32, 30);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(115, 138);
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.Image = Properties.Resources.pizza;
+            pictureBox2.Location = new Point(196, 30);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(118, 92);
+            pictureBox2.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox2.TabIndex = 1;
+            pictureBox2.TabStop = false;
+            pictureBox2.Click += pictureBox2_Click;
+            // 
+            // pictureBox3
+            // 
+            pictureBox3.Image = Properties.Resources.pasta;
+            pictureBox3.Location = new Point(362, 26);
+            pictureBox3.Name = "pictureBox3";
+            pictureBox3.Size = new Size(133, 92);
+            pictureBox3.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox3.TabIndex = 2;
+            pictureBox3.TabStop = false;
+            pictureBox3.Click += pictureBox3_Click;
+            // 
+            // pictureBox4
+            // 
+            pictureBox4.Image = Properties.Resources.salad;
+            pictureBox4.Location = new Point(569, 26);
+            pictureBox4.Name = "pictureBox4";
+            pictureBox4.Size = new Size(133, 92);
+            pictureBox4.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox4.TabIndex = 3;
+            pictureBox4.TabStop = false;
+            pictureBox4.Click += pictureBox4_Click;
+            // 
+            // pictureBox5
+            // 
+            pictureBox5.Image = Properties.Resources.steak;
+            pictureBox5.Location = new Point(569, 321);
+            pictureBox5.Name = "pictureBox5";
+            pictureBox5.Size = new Size(133, 92);
+            pictureBox5.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox5.TabIndex = 6;
+            pictureBox5.TabStop = false;
+            pictureBox5.Click += pictureBox5_Click;
+            // 
+            // pictureBox6
+            // 
+            pictureBox6.Image = Properties.Resources.seafood;
+            pictureBox6.Location = new Point(376, 321);
+            pictureBox6.Name = "pictureBox6";
+            pictureBox6.Size = new Size(133, 92);
+            pictureBox6.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox6.TabIndex = 5;
+            pictureBox6.TabStop = false;
+            pictureBox6.Click += pictureBox6_Click;
+            // 
+            // pictureBox7
+            // 
+            pictureBox7.Image = Properties.Resources.mainCourse;
+            pictureBox7.Location = new Point(196, 321);
+            pictureBox7.Name = "pictureBox7";
+            pictureBox7.Size = new Size(118, 92);
+            pictureBox7.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox7.TabIndex = 4;
+            pictureBox7.TabStop = false;
+            pictureBox7.Click += pictureBox7_Click;
+            // 
+            // pictureBox8
+            // 
+            pictureBox8.Image = Properties.Resources.gluten;
+            pictureBox8.Location = new Point(569, 482);
+            pictureBox8.Name = "pictureBox8";
+            pictureBox8.Size = new Size(133, 92);
+            pictureBox8.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox8.TabIndex = 9;
+            pictureBox8.TabStop = false;
+            pictureBox8.Click += pictureBox8_Click;
+            // 
+            // pictureBox9
+            // 
+            pictureBox9.Image = Properties.Resources.vegan;
+            pictureBox9.Location = new Point(376, 482);
+            pictureBox9.Name = "pictureBox9";
+            pictureBox9.Size = new Size(133, 92);
+            pictureBox9.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox9.TabIndex = 8;
+            pictureBox9.TabStop = false;
+            pictureBox9.Click += pictureBox9_Click;
+            // 
+            // pictureBox10
+            // 
+            pictureBox10.Image = Properties.Resources.vegetarian;
+            pictureBox10.Location = new Point(196, 482);
+            pictureBox10.Name = "pictureBox10";
+            pictureBox10.Size = new Size(118, 92);
+            pictureBox10.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox10.TabIndex = 7;
+            pictureBox10.TabStop = false;
+            pictureBox10.Click += pictureBox10_Click;
+            // 
+            // pictureBox11
+            // 
+            pictureBox11.Image = Properties.Resources.appetizer;
+            pictureBox11.Location = new Point(569, 166);
+            pictureBox11.Name = "pictureBox11";
+            pictureBox11.Size = new Size(133, 92);
+            pictureBox11.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox11.TabIndex = 12;
+            pictureBox11.TabStop = false;
+            pictureBox11.Click += pictureBox11_Click;
+            // 
+            // pictureBox12
+            // 
+            pictureBox12.Image = Properties.Resources.drink;
+            pictureBox12.Location = new Point(362, 166);
+            pictureBox12.Name = "pictureBox12";
+            pictureBox12.Size = new Size(133, 92);
+            pictureBox12.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox12.TabIndex = 11;
+            pictureBox12.TabStop = false;
+            pictureBox12.Click += pictureBox12_Click;
+            // 
+            // pictureBox13
+            // 
+            pictureBox13.Image = Properties.Resources.dessert;
+            pictureBox13.Location = new Point(196, 166);
+            pictureBox13.Name = "pictureBox13";
+            pictureBox13.Size = new Size(118, 92);
+            pictureBox13.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox13.TabIndex = 10;
+            pictureBox13.TabStop = false;
+            pictureBox13.Click += pictureBox13_Click;
+            // 
+            // listBox1
+            // 
+            listBox1.FormattingEnabled = true;
+            listBox1.Location = new Point(730, 26);
+            listBox1.Name = "listBox1";
+            listBox1.Size = new Size(201, 404);
+            listBox1.TabIndex = 13;
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.Red;
+            button1.Location = new Point(728, 468);
+            button1.Name = "button1";
+            button1.Size = new Size(103, 29);
+            button1.TabIndex = 14;
+            button1.Text = "Temizle";
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.BackColor = Color.FromArgb(255, 128, 0);
+            button2.Location = new Point(850, 468);
+            button2.Name = "button2";
+            button2.Size = new Size(94, 29);
+            button2.TabIndex = 15;
+            button2.Text = "Sil";
+            button2.UseVisualStyleBackColor = false;
+            // 
+            // button3
+            // 
+            button3.BackColor = Color.FromArgb(0, 192, 0);
+            button3.Location = new Point(730, 576);
+            button3.Name = "button3";
+            button3.Size = new Size(103, 34);
+            button3.TabIndex = 16;
+            button3.Text = "Hesabla";
+            button3.UseVisualStyleBackColor = false;
+            button3.Click += button3_Click;
+            // 
+            // button5
+            // 
+            button5.Location = new Point(23, 524);
+            button5.Name = "button5";
+            button5.Size = new Size(143, 50);
+            button5.TabIndex = 18;
+            button5.Text = "Qaligi sil";
+            button5.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            button6.Location = new Point(10, 388);
+            button6.Name = "button6";
+            button6.Size = new Size(156, 50);
+            button6.TabIndex = 19;
+            button6.Text = "Netice";
+            button6.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(21, 481);
+            label1.Name = "label1";
+            label1.Size = new Size(100, 20);
+            label1.TabIndex = 20;
+            label1.Text = "Qaliq mebleg";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(12, 321);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(143, 27);
+            textBox1.TabIndex = 21;
+            textBox1.Text = "0";
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(863, 583);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(100, 27);
+            textBox2.TabIndex = 22;
+            textBox2.Text = "0";
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(863, 536);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(100, 27);
+            textBox3.TabIndex = 23;
+            textBox3.Text = "0";
+            textBox3.TextChanged += textBox3_TextChanged;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(730, 543);
+            label2.Name = "label2";
+            label2.Size = new Size(55, 20);
+            label2.TabIndex = 24;
+            label2.Text = "Odenis";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.Bisque;
+            ClientSize = new Size(982, 622);
+            Controls.Add(label2);
+            Controls.Add(textBox3);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(label1);
+            Controls.Add(button6);
+            Controls.Add(button5);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(listBox1);
+            Controls.Add(pictureBox11);
+            Controls.Add(pictureBox12);
+            Controls.Add(pictureBox13);
+            Controls.Add(pictureBox8);
+            Controls.Add(pictureBox9);
+            Controls.Add(pictureBox10);
+            Controls.Add(pictureBox5);
+            Controls.Add(pictureBox6);
+            Controls.Add(pictureBox7);
+            Controls.Add(pictureBox4);
+            Controls.Add(pictureBox3);
+            Controls.Add(pictureBox2);
+            Controls.Add(pictureBox1);
+            Name = "Form1";
+            Text = "Restaurant";
+            Load += Form1_Load;
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox4).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox5).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox6).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox7).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox8).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox9).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox10).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox11).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox12).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox13).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private PictureBox pictureBox1;
+        private PictureBox pictureBox2;
+        private PictureBox pictureBox3;
+        private PictureBox pictureBox4;
+        private PictureBox pictureBox5;
+        private PictureBox pictureBox6;
+        private PictureBox pictureBox7;
+        private PictureBox pictureBox8;
+        private PictureBox pictureBox9;
+        private PictureBox pictureBox10;
+        private PictureBox pictureBox11;
+        private PictureBox pictureBox12;
+        private PictureBox pictureBox13;
+        private ListBox listBox1;
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Button button5;
+        private Button button6;
+        private Label label1;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private TextBox textBox3;
+        private Label label2;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,180 @@
+﻿namespace Restaurant
+{
+    public partial class Form1 : Form
+    {
+        string pizza = "Pizza";
+        string pasta = "Pasta";
+        string salad = "Salad";
+        string dessert = "Dessert";
+        string drink = "Drink";
+        string appetizer = "Appetizer";
+        string mainCourse = "Main Course";
+        string seafood = "Seafood";
+        string steak = "Steak";
+        string vegetarian = "Vegetarian";
+        string vegan = "Vegan";
+        string glutenFree = "Gluten Free";
+
+        double pizzaPrice = 8.99;
+        double pastaPrice = 10.99;
+        double saladPrice = 6.99;
+        double dessertPrice = 4.99;
+        double drinkPrice = 2.99;
+        double appetizerPrice = 5.99;
+        double mainCoursePrice = 12.99;
+        double seafoodPrice = 15.99;
+        double steakPrice = 18.99;
+        double vegetarianPrice = 9.99;
+        double veganPrice = 11.99;
+        double glutenFreePrice = 13.99;
+        double totalPrice = 0.0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox9_Click(object sender, EventArgs e)
+        {
+            totalPrice += veganPrice;
+            listBox1.Items.Add("Vegan :11.99");
+            textBox1.Text = totalPrice.ToString("0.00");
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+            totalPrice += pizzaPrice;
+            listBox1.Items.Add("Pizza :6");
+            textBox1.Text = totalPrice.ToString("0.00");
+        }
+
+        private void pictureBox3_Click(object sender, EventArgs e)
+        {
+            totalPrice += pastaPrice;
+            listBox1.Items.Add("Pasta :10.99");
+            textBox1.Text = totalPrice.ToString("0.00");
+        }
+
+        private void pictureBox4_Click(object sender, EventArgs e)
+        {
+            totalPrice += saladPrice;
+            listBox1.Items.Add("Salad :6.99");
+            textBox1.Text = totalPrice.ToString("0.00");
+        }
+
+        private void pictureBox13_Click(object sender, EventArgs e)
+        {
+            totalPrice += dessertPrice;
+            listBox1.Items.Add("Dessert :4.99");
+            textBox1.Text = totalPrice.ToString("0.00");
+        }
+
+        private void pictureBox12_Click(object sender, EventArgs e)
+        {
+            totalPrice += drinkPrice;
+            listBox1.Items.Add("Drink :2.99");
+            textBox1.Text = totalPrice.ToString("0.00");
+        }
+
+        private void pictureBox11_Click(object sender, EventArgs e)
+        {
+            totalPrice += appetizerPrice;
+            listBox1.Items.Add("Appetizer :5.99");
+            textBox1.Text = totalPrice.ToString("0.00");
+        }
+
+        private void pictureBox7_Click(object sender, EventArgs e)
+        {
+            totalPrice += mainCoursePrice;
+            listBox1.Items.Add("Main Course :12.99");
+            textBox1.Text = totalPrice.ToString("0.00");
+        }
+
+        private void pictureBox6_Click(object sender, EventArgs e)
+        {
+            totalPrice += seafoodPrice;
+            listBox1.Items.Add("Seafood :15.99");
+            textBox1.Text = totalPrice.ToString("0.00");
+        }
+
+        private void pictureBox5_Click(object sender, EventArgs e)
+        {
+            totalPrice += steakPrice;
+            listBox1.Items.Add("Steak :18.99");
+            textBox1.Text = totalPrice.ToString("0.00");
+        }
+
+        private void pictureBox10_Click(object sender, EventArgs e)
+        {
+            totalPrice += vegetarianPrice;
+            listBox1.Items.Add("Vegetarian :9.99");
+            textBox1.Text = totalPrice.ToString("0.00");
+        }
+
+        private void pictureBox8_Click(object sender, EventArgs e)
+        {
+            totalPrice += dessertPrice;
+            listBox1.Items.Add("Gluten Free :13.99");
+            textBox1.Text = totalPrice.ToString("0.00");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+
+            if (textBox3.Text != "") 
+            {
+                try
+                {
+                    double odenis = Convert.ToDouble(textBox3.Text);
+                    double umumi = totalPrice;
+
+                    if (odenis >= umumi)
+                    {
+                        double qaliq = odenis - umumi;
+
+                        textBox2.Text = qaliq.ToString("0.00");
+
+                        listBox1.Items.Add("------------------");
+                        listBox1.Items.Add("Ümumi: " + umumi.ToString("0.00") + " AZN");
+                        listBox1.Items.Add("Ödəniş: " + odenis.ToString("0.00") + " AZN");
+                        listBox1.Items.Add("Qalıq: " + qaliq.ToString("0.00") + " AZN");
+                    }
+                    else
+                    {
+                        MessageBox.Show("Pul çatmır!", "Diqqət");
+                    }
+                }
+                catch
+                {
+                    MessageBox.Show("Zəhmət olmasa düzgün rəqəm yaz!");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Ödəniş məbləğini yazın!");
+            }
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+
+            
+            totalPrice = 0.0;
+            textBox1.Text = "";  
+            textBox2.Text = ""; 
+            textBox3.Text = "";
+        }
+    }
+}
+
